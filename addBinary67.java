2eb5474812b5abd5a04a66ed9655b5169428d9ff
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-02-18 17:35:44 +0530

    add

--- addBinary67.java
+++ addBinary67.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class addBinary67 {
     public static String addBinary(String a, String b) {
@@ -10,11 +11,23 @@ public class addBinary67 {
         return binary;
 
     }
+
+    public static String addLargeNumber(String a, String b) {
+        BigInteger num1 = new BigInteger(a,2);
+        BigInteger num2 = new BigInteger(b,2);
+        BigInteger sum= num1.add(num2);
+        String binary = sum.toString(2);
+        return binary;
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
         String a= sc.nextLine();
         String b= sc.nextLine();
-        String result = addBinary(a, b);
-        System.out.println(result);
+        // String result = addBinary(a, b);
+
+        // System.out.println(result);
+        String Large = addLargeNumber(a,b);
+        System.out.println(Large);
     }
 }
