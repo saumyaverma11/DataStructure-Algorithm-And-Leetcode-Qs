53c75e17a0c24a51e4b6b249edb3436ef471bfc4
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-03-01 10:45:03 +0530

    add

---
+++ ApplyOprOnAnArray2468.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ApplyOprOnAnArray2468{
+    public static int[] applyOperations(int[] nums) {
+        int n= nums.length;
+        
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1] && nums[i]!=0){
+                nums[i] = nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+      
+        int low=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i]= nums[low];
+                nums[low]= temp;
+                low++;
+            }
+        }
+      return nums;
+    }   
+    public static void main(String[] args) {
+        int arr[]={1,2,2,1,1,0};
+        int result [] = applyOperations(arr);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
+
