b7771190f8b13331da25baec560f7e2cd4848d46
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-04-01 19:21:12 +0530

    add

--- unionANDintersection349.java
+++ unionANDintersection349.java
@@ -4,27 +4,40 @@ public class unionANDintersection349 {
     public static void unionandIntersection(int nums1[], int nums2[])
 {
     HashSet<Integer> set = new HashSet<>();
+    int c1= 0;
     for(int i =0;i<nums1.length;i++){
-        set.add(nums1[i]);
+        
+        if(set.add(nums1[i])){
+            c1++;
+        };
     }
+    
     for(int i =0;i<nums2.length;i++){
-        set.add(nums2[i]);
+        
+        if(set.add(nums2[i])){
+            c1++;
+        };
     }
-    System.out.println(set);
+    System.out.println(set + "and count of union is : " + c1);
 
         set.clear();
 
         HashSet<Integer> set2 = new HashSet<>();
+        int c2=0;
         for(int i =0;i<nums1.length;i++){
             set.add(nums1[i]);
         }
+
         for(int i =0;i<nums2.length;i++){
             if(set.contains(nums2[i])){
-                set2.add(nums2[i]);
+    
+                if(set2.add(nums2[i])){
+                    c2++;
+                };
             }
         }
     
-    System.out.println(set2);
+    System.out.println(set2 +" and count of intersection is :" + c2);
 }
         public static void main(String[] args) {
             // int arr1[]={7,3,9};
