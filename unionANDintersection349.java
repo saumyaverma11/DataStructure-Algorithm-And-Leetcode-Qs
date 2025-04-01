77041879c417b6123d7103cacbe41fd2e9c7e53b
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-04-01 19:15:45 +0530

    add

--- unionANDintersection349.java
+++ unionANDintersection349.java
@@ -14,16 +14,17 @@ public class unionANDintersection349 {
 
         set.clear();
 
-    for(int i =0;i<nums1.length;i++){
-        set.add(nums1[i]);
-    }
-
-    for(int i =0;i<nums2.length;i++){
-        if(!set.contains(nums2[i])){
-            set.remove(nums2[i]);
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i =0;i<nums1.length;i++){
+            set.add(nums1[i]);
         }
-    }
-    System.out.println(set);
+        for(int i =0;i<nums2.length;i++){
+            if(set.contains(nums2[i])){
+                set2.add(nums2[i]);
+            }
+        }
+    
+    System.out.println(set2);
 }
         public static void main(String[] args) {
             // int arr1[]={7,3,9};
