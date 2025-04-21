8a153bc2447278545645d18320de0743e7c407f4
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-04-22 00:28:08 +0530

    add

---
+++ Arrays/numberOfEmployeesWhoMetTheTarget2798.java
@@ -0,0 +1,19 @@
+
+public class numberOfEmployeesWhoMetTheTarget2798 {
+
+    public static int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        for(int i =0;i<hours.length;i++){
+            if(hours[i] >= target){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int hours[]= {0,1,2,3,4};
+        int target =2;
+        int res = numberOfEmployeesWhoMetTarget(hours, target);
+        System.out.println(res);
+    }
+}
