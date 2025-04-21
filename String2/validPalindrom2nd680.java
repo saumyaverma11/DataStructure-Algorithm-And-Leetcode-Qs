27750935484a5e57c2f61f0bd3d8421d5bfcf37a
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-04-22 01:17:53 +0530

    add

--- String2/validPalindrom2nd680.java
+++ String2/validPalindrom2nd680.java
@@ -26,9 +26,41 @@ public class validPalindrom2nd680 {
         return true;
     }
 
+    public static boolean alternativeMyApp(String s) {
+
+        int l =0;
+        int r = s.length()-1;
+
+        while(l<=r)
+        {  
+            if(s.charAt(l)!=s.charAt(r))
+            {
+                           if(IsPalindrome(s.toString(),l+1,r))
+                            {
+                           return true;
+                            }
+                            else if(IsPalindrome(s.toString(),l,r-1))
+                             {
+                                  return true;
+                             }
+                             else{
+                                return false;
+                             }               
+            }
+            
+            l++;
+            r--;
+        }
+
+
+                
+        return true;
+    }
     public static void main(String[] args) {
         String s = "abca";
         boolean res = validPalindrome(s);
         System.out.println(res);
+        boolean res2 = alternativeMyApp(s);
+        System.out.println(res2);
     }
 }
