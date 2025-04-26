19c73e359f6c5fb13a649455709c52d8e08ae5c2
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-04-27 00:58:12 +0530

    add

--- maximuNumOfWOrdsFoundInSentences2114.java
+++ maximuNumOfWOrdsFoundInSentences2114.java
@@ -1,7 +1,6 @@
 public class maximuNumOfWOrdsFoundInSentences2114 {
     public static int mostWordsFound(String[] sentences) {
         int max=0;
-        // String arr[]= sentences.split(",");
         for(int i=0;i<sentences.length;i++){
             String word1[] = sentences[i].split(" ");
             max= Math.max(max,word1.length);
