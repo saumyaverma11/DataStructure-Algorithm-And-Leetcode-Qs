cfdca97dfd733961b43132758494af30443ef97c
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-01 19:04:57 +0530

    add

--- distributiveCandiesAmongChildren2nd2929.java
+++ distributiveCandiesAmongChildren2nd2929.java
@@ -1,7 +1,9 @@
 
 public class distributiveCandiesAmongChildren2nd2929 {
 
-    public static int totalCandies(int n,int l){
+    //O(n3)
+
+    public static int totalCandiesBrute(int n,int l){
         int count=0;
         for(int i=0;i<=l;i++){
             for(int j=0;j<=l;j++){
@@ -14,10 +16,51 @@ public class distributiveCandiesAmongChildren2nd2929 {
         }
         return count;
     }
+
+
+    //O(n2)
+
+    public static int totalCandiesBetter(int n, int l){
+        int count=0;
+        for(int i=0;i<=l;i++){
+            for(int j=0;j<=l;j++){
+                int k = (n-i-j);
+                if(k>=0 && k<=l){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    //O(n)
+
+     public static long totalCandiesEfficient(int n, int limit) {
+        long ans=0;
+        for(int i=0;i<=Math.min(n,limit);i++){
+            int rem = n-i;
+            if(rem > 2*limit)
+            continue;
+
+            int count=0;
+            int j = Math.max(0,rem - limit);
+            int k = Math.min(rem , limit);
+            ans += k-j+1;
+
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int  n=5;
         int limit=2;
-        int res = totalCandies(n,limit);
+        int res = totalCandiesBrute(n,limit);
         System.out.println(res);
+        int res2 = totalCandiesBetter(n, limit);
+        System.out.println(res2);
+        long res3 = totalCandiesEfficient(n, limit);
+        System.out.println(res3);
     }
+
+
 }
