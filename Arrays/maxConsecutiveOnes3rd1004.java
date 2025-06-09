17adec0ff4ce22a3178b82683d49ab1dcf43370c
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-09 23:21:56 +0530

    add

---
+++ Arrays/maxConsecutiveOnes3rd1004.java
@@ -0,0 +1,23 @@
+public class maxConsecutiveOnes3rd1004 {
+     public static int longestOnes(int[] nums, int k) {
+        int window = 0;
+        int ans =0;
+        int left =0;
+        int n= nums.length;
+        for(int right=0;right<n;right++){
+            window = window + nums[right];
+            while(window + k < right - left +1){
+               window = window - nums[left];
+               left++;
+            }
+            ans = Math.max(ans,right-left+1);
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        int nums[]= {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+        int k=3;
+        int res = longestOnes(nums,k);
+        System.out.println(res);
+    }
+}
