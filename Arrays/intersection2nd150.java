c894c434dcc4a1fd310292ea8a50536cdc706dcd
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-14 00:18:34 +0530

    Add

---
+++ Arrays/intersection2nd150.java
@@ -0,0 +1,33 @@
+public class intersection2nd150 {
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+            return new int[0]; // Return empty array if either input is null or empty
+        }
+
+        // Use a HashMap to count occurrences of each number in nums1
+      Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        // Count frequency of each number in nums1
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Traverse nums2 and collect matches based on frequency
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        // Convert list to int[]
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(i);
+        }
+
+        return res;
+    }
+}
