0acd4c3acecbe782efc9fbfbd23dbdc4c7d75eb8
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-17 00:44:54 +0530

    add

---
+++ sortList148.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while(fast !=null && fast.next !=null){
+            prev= slow;
+            slow= slow.next;
+            fast= fast.next.next;
+        }
+
+        if(prev!=null) prev.next= null;
+        return slow;
+    }
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr= dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                curr.next= l1;
+                l1= l1.next;
+            }
+            else{
+                curr.next = l2;
+                l2= l2.next;
+            }
+            curr= curr.next;
+        }
+        curr.next = (l1!=null)? l1 : l2;
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {
+       if(head== null || head.next == null){
+        return head;
+       }
+
+       ListNode mid = getMid(head);
+       ListNode left = sortList(head);
+       ListNode right = sortList(mid);
+
+
+       return merge(left,right);
+    }
+}
