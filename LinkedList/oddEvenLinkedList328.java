55ba9a001df89b98cd6e3024a05dcaf18a72a01a
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-21 23:52:37 +0530

    add

---
+++ LinkedList/oddEvenLinkedList328.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val= val;
+        this.next= null;
+    }
+}
+
+
+public class oddEvenLinkedList328 {
+
+     public static Node oddEvenList(Node head) {
+        Node oddList = new Node(0);
+        if(head== null){
+            return null;
+        }
+        Node evenList = new Node(0);
+
+        Node oddtail = oddList;
+        Node eventail = evenList;
+
+        Node temp = head;
+        int count=1;
+        while(temp!=null){
+            if(count%2!=0){
+                oddtail.next= temp;
+                oddtail= oddtail.next;
+            }
+            else{
+                eventail.next= temp;
+                eventail= eventail.next;
+            }
+            temp= temp.next;
+            count++;
+        }
+        
+        eventail.next = null;          //terminate the even , meand found the end.
+        oddtail.next = evenList.next;
+        
+        return oddList.next;
+    }
+    public static void main(String[] args) {
+        // Example usage
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of nodes in the linked list:");
+        int n = sc.nextInt();
+        Node head= null;
+        Node tail= null;
+        for(int i=0;i<n;i++){
+            Node newNode = new Node(sc.nextInt());
+            if(head == null){
+                head = tail = newNode;
+            }
+            else{
+                tail.next = newNode;
+                tail = tail.next;
+            }
+        }
+        Node result = oddEvenList(head);
+        System.out.println("Rearranged linked list:");
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
