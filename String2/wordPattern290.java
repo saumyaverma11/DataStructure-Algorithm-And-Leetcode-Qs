17284cea4151c94015bf67a0b49ba7f9e0bdc09a
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-27 00:00:29 +0530

    add

---
+++ String2/wordPattern290.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class wordPattern290{
+    public boolean wordPattern(String pattern, String s) {
+        String newStr[] = s.split(" ");
+        HashMap<Character,String> hm= new HashMap<>();
+        for(int i=0;i<pattern.length(); )
+    }
+
+    public static void main(String args[]){
+        String pattern ="abba";
+        String s = "dog cat cat dog";
+
+    }
+}
