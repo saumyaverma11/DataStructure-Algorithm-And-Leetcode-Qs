7435fdb03e0b4ccd9f26912e50c362e2c5be6978
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-07-06 00:02:15 +0530

    add

--- Arrays/luckyinteger1394.java
+++ Arrays/luckyinteger1394.java
@@ -1,7 +1,30 @@
 import java.util.*;
 public class luckyinteger1394
 {
-    public static void main(String[] args) {
+      public static int findLucky(int[] arr) {
+        int hash[]= new int[501];
+        for(int i=0;i<arr.length;i++){
+            hash[arr[i]]++;
+        }
+        boolean flag = true;
+        int max= Integer.MIN_VALUE;
+        for(int i=1;i<hash.length;i++){
+            if ( i==hash[i]){
+                flag = false;
+                max = Math.max(max,hash[i]);
+            }
+        }
+        if(!flag){
+            return max;
+        }
+        else{
+            return -1;
+        }
         
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,4};
+        int res = findLucky(arr);
+        System.out.println(res);
     }
 }
