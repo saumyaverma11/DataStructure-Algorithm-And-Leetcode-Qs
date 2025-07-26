421490aae5071f146bfc784336d6663f96e1711f
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-07-27 00:05:08 +0530

    add

--- Sumtroot.java
+++ Sumtroot.java
@@ -1,3 +1,24 @@
 public class Sumtroot{
+ boolean found = false;
+    public void path(TreeNode root, int res, int targetSum){
+        if(root==null){
+            return;
+        }
+        res= res + root.val;
 
+
+        if(root.left ==null && root.right ==null && res == targetSum ){
+            found = true;
+            return;
+        }
+        path(root.left,res,targetSum);
+        path(root.right,res,targetSum);
+     
+
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        int res=0;
+        path(root,res,targetSum);
+        return found;
+    }
 }
